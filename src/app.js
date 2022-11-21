d94ddf9c9cec0785294c42cd54059691001905f2
NapoZL <52946545+NapoZL@users.noreply.github.com>
2022-11-20 18:36:40 -0600

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+// Mettings
+    app.set('port', 4000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+// Middlewares
+    app.use(morgan('dev'));
+    app.use(express.urlencoded({extended: false}));
+
+// Routes
+    app.use(require('./routes/index'));
+
+// Static 
+    app.use(express.static(path.join(__dirname, 'public')));
+
+// 404 handler
+    app.use((req, res, next) =>{
+        res.status(404).send('404 not found');
+    })
+
+module.exports = app;
